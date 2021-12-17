a364bfc006fc3eb132aa242732de8c1d572e3a49
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-17 12:27:55 +0800

    Delete src directory

--- src/services.py
+++
@@ -1,9 +0,0 @@
-from src.models import Lead
-
-
-def create_lead(*, email: str, source: str, ) -> Lead:
-    lead = Lead(email=email, source=source)
-    lead.full_clean()
-    lead.save()
-
-    return lead
